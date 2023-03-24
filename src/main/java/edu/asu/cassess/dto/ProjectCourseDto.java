4ffc27bdd6485d20c06709596f7d4eae41ba6eda
Srikanth <sbaktha1@asu.edu>
2023-03-24 15:15:56 -0700

    US-52-TASK-88 Add Project and course info in the Project repo

---
+++ src/main/java/edu/asu/cassess/dto/ProjectCourseDto.java
@@ -0,0 +1,15 @@
+package edu.asu.cassess.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectCourseDto {
+  private String teamName;
+  private String courseName;
+  private Long projectId;
+  private String taigaToken;
+}
--- src/main/java/edu/asu/cassess/persist/repo/taiga/ProjectRepo.java
+++ src/main/java/edu/asu/cassess/persist/repo/taiga/ProjectRepo.java
@@ -1,9 +1,23 @@
 package edu.asu.cassess.persist.repo.taiga;
 
 import edu.asu.cassess.persist.entity.taiga.Project;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
+@Repository
 public interface ProjectRepo extends JpaRepository<Project, String> {
+
+  @Query(value = "Select t.team_name, t.course as course_name, p.id as project_id, c.taiga_token \n"
+      + "from project p Join teams t on t.taiga_project_slug = p.slug Join courses c on t.course = c.course \n"
+      + "where c.end_date >= curdate()", nativeQuery = true)
+  List<Object[]> getProjectInfoOfActiveCourses();
+
+  @Query(value = "Select t.team_name, t.course as course_name, p.id as project_id, c.taiga_token \n"
+      + "from project p Join teams t on t.taiga_project_slug = p.slug Join courses c on t.course = c.course \n"
+      + "where t.course = ?1", nativeQuery = true)
+  List<Object[]> getProjectCourseInfoByCourse(String course);
 }
