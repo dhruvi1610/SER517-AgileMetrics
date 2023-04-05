a2c3abdbef24724b6c17c401bdf98cce4f019444
geethika-n <90439924+geethika-n@users.noreply.github.com>
2023-04-05 16:29:19 -0700

    US-94-TASK-111 DTO classes for Custom attribute and custom attribute values

---
+++ src/main/java/edu/asu/cassess/dto/sprint/CustomAttributesDto.java
@@ -0,0 +1,21 @@
+package edu.asu.cassess.dto.sprint;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CustomAttributesDto {
+
+  @JsonProperty("id")
+  private Long attributeId;
+
+  @JsonProperty("name")
+  private String attributeName;
+}
