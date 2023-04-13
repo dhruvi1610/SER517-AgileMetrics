acf9dbdff4fc9d197ffbbf24565a22487b21eb42
geethika-n <90439924+geethika-n@users.noreply.github.com>
2023-04-12 23:37:40 -0700

    US-97-TASK-131 Dto classes for commit and commit info for git blame

---
+++ src/main/java/edu/asu/cassess/dto/github/external/CommitDto.java
@@ -0,0 +1,20 @@
+package edu.asu.cassess.dto.github.external;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CommitDto {
+
+  @JsonProperty("sha")
+  private String commitId;
+
+  @JsonProperty("commit")
+  private CommitInfoDto commitInfo;
+}
